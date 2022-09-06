69f22ac397e66320a0cf96f06ced021a4a907b2d
Panagnimba <panagnimba@gmail.com>
2022-09-06 20:12:22 +0200

    Banner version 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/axios'],
   axios: {
-    baseURL: 'http://serverticket.herokuapp.com', // Used as fallback if no runtime config is provided
+    baseURL: 'https://serverticket.herokuapp.com', // Used as fallback if no runtime config is provided
     // baseURL: 'http://localhost:9000', // Used as fallback if no runtime config is provided
     credentials: true
   },
