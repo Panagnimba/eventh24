4d9370b5ac37a363c2f00de0800dc9c7ea8ade6b
Panagnimba <panagnimba@gmail.com>
2022-11-18 14:56:34 +0100

    new server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-let server_url = process.env.NODE_ENV !== 'production' ? 'http://localhost:9000' : 'https://vinoticket.herokuapp.com/';
+let server_url =  process.env.NODE_ENV !== 'production' ? 'http://localhost:9000' : 'https://apivinoticket.herokuapp.com';
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
