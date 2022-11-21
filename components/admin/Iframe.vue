a4070960cffaea279f21afb5856c9bae120619fa
Panagnimba <panagnimba@gmail.com>
2022-11-21 14:34:44 +0100

    sarl and new server url

--- components/admin/Iframe.vue
+++ components/admin/Iframe.vue
@@ -1,19 +1,11 @@
 <template>
   <client-only>
     <div class="m-5">
-      <iframe :src="link" frameborder="0" class="w-full h-screen"></iframe>
+      <iframe
+        src="https://www.vinoticket.com"
+        frameborder="0"
+        class="w-full h-full"
+      ></iframe>
     </div>
   </client-only>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      link: null,
-    };
-  },
-  mounted() {
-    this.link = window.location.hostname;
-  },
-};
-</script>
